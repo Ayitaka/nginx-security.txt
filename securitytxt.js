093714b787a08f7eb1fb0de68b9e9bb449df43af
Ayitaka <22988712+Ayitaka@users.noreply.github.com>
2023-01-12 02:18:38 -0800

    Code formatting

--- securitytxt.js
+++ securitytxt.js
@@ -1,10 +1,12 @@
 function pad(number) {
     var r = String(number);
+
     if (r.length === 1) {
-    r = '0' + r;
-  }
-  return r;
+        r = '0' + r;
+    }
+    return r;
 }
+
 //Modifying the prototype so it adds one year to expiration data
 Date.prototype.toISOString = function() {
     return (this.getUTCFullYear() + 1) +
@@ -16,8 +18,10 @@ Date.prototype.toISOString = function() {
     '.' + String((this.getUTCMilliseconds() / 1000).toFixed(3)).slice(2, 5) +
     'Z';
 }
+
 function setexpires(r) {
-var date = new Date();
-return ("Expires: " + date.toISOString() + "\n");
+    var date = new Date();
+    return ("Expires: " + date.toISOString() + "\n");
 }
+
 export default {setexpires};
